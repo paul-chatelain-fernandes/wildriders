21b44aa9876f0fa3c80834e6b9a4c33fa8869991
Dangiras <dan.hausmans@gmail.com>
2023-01-13 09:25:44 +0100

    Settings finished for both

--- app/company/settings/page.tsx
+++ app/company/settings/page.tsx
@@ -1,10 +1,64 @@
-import NavbarCompany from "../../NavbarCompany";
+"use client";
+import NavbarUser from "../../NavbarCompany";
+import {
+  Stack,
+  Box,
+  Text,
+  Flex,
+  Heading,
+  Image,
+  HStack,
+  VStack,
+  FormControl,
+  Input,
+  FormLabel,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function Page() {
+function ResourcesPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <>
-      <NavbarCompany />
-      <h1>Company settings</h1>
+      <NavbarUser />
+      <Stack minH={"85vh"}>
+        <Flex justifyContent={"center"}>
+          <Heading p={4}>Modify your Company</Heading>
+        </Flex>
+        <Flex justifyContent={"center"}>
+          <Box p={4}>
+            <FormControl>
+              <FormLabel>Company Name</FormLabel>
+              <Input
+                width={"20rem"}
+                placeholder="Amazon"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
+                placeholder="John@gmail.com"
+                width={"20rem"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+            <Button
+              backgroundColor={"pink.600"}
+              mt={4}
+              onClick={() => alert("Saved Changes!")}
+            >
+              Save Changes
+            </Button>
+          </Box>
+        </Flex>
+      </Stack>
     </>
   );
 }
+
+export default ResourcesPage;
